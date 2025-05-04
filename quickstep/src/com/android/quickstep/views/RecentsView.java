DerpFest-AOSP/packages_apps_DerpLauncher

DerpLauncher: Skip overview scale animation on tablet
* Calling doScrollScale() on a tablet causes the app to become "small" and then expands to fit the screen when scrolling
* Bug has been reproduced on nabu(Xiaomi Pad 5), pipa(Xiaomi Pad 6) and gta4xl(Samsung Tab S6 Lite)
* Original patch was by kfkcome(https://github.com/Kfkcome/android_packages_apps_Launcher3/commit/69c0319666cbb84e34a65cb770c4c4188253d50c)

Co-authored-by: Pranav Vashi <neobuddy89@gmail.com>
Signed-off-by: Pranav Vashi <neobuddy89@gmail.com>

quickstep/src/com/android/quickstep/views/RecentsView.java
@@ -6077,7 +6077,7 @@ public abstract class RecentsView<ACTIVITY_TYPE extends StatefulActivity<STATE_T
     }
 
     private void doScrollScale() {
-        if (showAsGrid())
+        if (showAsGrid() || mActivity.getDeviceProfile().isTablet)
             return;
 
         //nick@lmo-20231004 if rotating launcher is enabled, rotation works differently

DerpFest-LOS/android_packages_apps_DerpLauncher

Change-Id: I0cfbaf7d7b3adb73efefe3a77b863c0a7df1e959
(cherry picked from commit 27651fbe10145aef850adc1da2f3cb0678054108)

quickstep/src/com/android/quickstep/views/RecentsView.java
@@ -6691,7 +6691,7 @@ public abstract class RecentsView<
     }
 
     private void doScrollScale() {
-        if (showAsGrid())
+        if (showAsGrid() || mContainer.getDeviceProfile().isTablet)
             return;
 
         boolean isInLandscape = mOrientationState.getTouchRotation() != ROTATION_0